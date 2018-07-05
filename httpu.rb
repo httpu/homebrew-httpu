a247f4f69233fd574cc42b7968fe51e3d53e2f61
Harry Lawrence <hazbo@gmx.com>
2018-07-05 14:08:11 +0100

    Updated to correct download

--- httpu.rb
+++ httpu.rb
@@ -1,7 +1,7 @@
 class Ssedit < Formula
   desc "the terminal-first http client"
   homepage "https://github.com/hazbo/httpu"
-  url "https://github.com/hazbo/httpu/releases/download/v0.0.1/ssedit-0.0.1.tar.gz"
+  url "https://github.com/hazbo/httpu/releases/download/0.0.1/httpu-0.0.1.tar.gz"
   sha256 "8231eb00b7f3ab916c2e7555a9804cea14c8bdb79f03ff003d84e552c154d6d7"
 
   bottle :unneeded
