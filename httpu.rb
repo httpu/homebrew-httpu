8e4a6599253831ccc06bbc8e5767e54f001a7995
Harry Lawrence <hazbo@gmx.com>
2018-07-05 13:40:17 +0100

    Initial

---
+++ httpu.rb
@@ -0,0 +1,16 @@
+class Ssedit < Formula
+  desc "the terminal-first http client"
+  homepage "https://github.com/hazbo/httpu"
+  url "https://github.com/hazbo/httpu/releases/download/v0.0.1/ssedit-0.0.1.tar.gz"
+  sha256 "8231eb00b7f3ab916c2e7555a9804cea14c8bdb79f03ff003d84e552c154d6d7"
+
+  bottle :unneeded
+
+  def install
+    bin.install "httpu"
+  end
+
+  test do
+    system "#{bin}/httpu", "version"
+  end
+end
