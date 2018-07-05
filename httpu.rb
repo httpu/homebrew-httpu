155005d657dff75a368d5e693b9e5a3393d1531b
Harry Lawrence <hazbo@gmx.com>
2018-07-05 14:17:55 +0100

    Updated sha

--- httpu.rb
+++ httpu.rb
@@ -2,7 +2,7 @@ class Httpu < Formula
   desc "the terminal-first http client"
   homepage "https://github.com/hazbo/httpu"
   url "https://github.com/hazbo/httpu/releases/download/0.0.1/httpu-0.0.1.tar.gz"
-  sha256 "8231eb00b7f3ab916c2e7555a9804cea14c8bdb79f03ff003d84e552c154d6d7"
+  sha256 "8615dfe5b3f1b511b5b5477cfd71e64df4b63d805aa0d387a59e558be55dcaaa"
 
   bottle :unneeded
 
