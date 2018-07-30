a328b8b84ee7b5d2ddfe3ac1d96c7fcfb4ced330
Harry Lawrence <harry@Harrys-MacBook-Pro.local>
2018-07-30 11:18:06 +0100

    Fix for release v0.0.2

--- httpu.rb
+++ httpu.rb
@@ -1,8 +1,8 @@
 class Httpu < Formula
   desc "the terminal-first http client"
   homepage "https://github.com/hazbo/httpu"
-  url "https://github.com/hazbo/httpu/releases/download/0.0.2/httpu.tar-0.0.2.gz"
-  sha256 "0912ae0d8101c066e5bb3d85fd85119bb636d8ad80535e182c900625c6cfe8af"
+  url "https://github.com/hazbo/httpu/releases/download/0.0.2/httpu-0.0.2.tar.gz"
+  sha256 "161130da561459c6546270a196f4b3a038a42825e168ef8f48837408d1c60cdb"
 
   bottle :unneeded
 
